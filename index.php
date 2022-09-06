11e9e45098648bad2ff2b762638fb870333256b5
Junbro0708 <aktm708@naver.com>
2022-09-06 16:27:43 -0700

    initialize

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- Javascript -->
+  <script defer type="module" src="./src/js/index.js"></script>
+  <!-- CSS -->
+  <link rel="stylesheet" href="./src/css/index.css">
+</head>
+<body>
+  <nav>
+    <div class="btn-group">
+      <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Action</button>
+      <ul class="dropdown-menu country">
+        <li>
+          <a class="dropdown-item" href=".?lang=kr">
+            <aside class="kr"></aside>
+            <span>Kr</span>
+          </a>
+        </li>
+        <li>
+          <a class="dropdown-item" href=".?lang=ca">
+            <aside class="ca"></aside>
+            <span>Ca</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <main>
+
+  </main>
+</body>
+</html>
